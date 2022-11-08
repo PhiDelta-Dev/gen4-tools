18b14961df9a96f410b0892c61320f8f600fb791
PhiDelta <74007613+PhiDelta-Dev@users.noreply.github.com>
2022-11-08 09:48:02 +0100

    Update pokemon_data.hpp

--- pkm/pokemon_data.hpp
+++ pkm/pokemon_data.hpp
@@ -9,18 +9,18 @@ namespace gen4::pkm
 	{
 		struct IVs
 		{
-			ui8 hp				{ 0 };
-			ui8 attack			{ 0 };
-			ui8 defense			{ 0 };
+			ui8 hp			{ 0 };
+			ui8 attack		{ 0 };
+			ui8 defense		{ 0 };
 			ui8 special_attack	{ 0 };
 			ui8 special_defense	{ 0 };
-			ui8 speed			{ 0 };
+			ui8 speed		{ 0 };
 		};
 
 		struct HiddenPower
 		{
 			ui8 type_id { 0 };
-			ui8 power	{ 0 };
+			ui8 power { 0 };
 		};
 
 	private:
@@ -64,21 +64,21 @@ namespace gen4::pkm
 
 		//Generate the IVs
 		t_lcg.advance();
-		m_ivs.hp				= (t_lcg.get_value() >> 16) & 0x1F;
-		m_ivs.attack			= (t_lcg.get_value() >> 21) & 0x1F;
-		m_ivs.defense			= (t_lcg.get_value() >> 26) & 0x1F;
+		m_ivs.hp		= (t_lcg.get_value() >> 16) & 0x1F;
+		m_ivs.attack		= (t_lcg.get_value() >> 21) & 0x1F;
+		m_ivs.defense		= (t_lcg.get_value() >> 26) & 0x1F;
 		t_lcg.advance();
 		m_ivs.special_attack	= (t_lcg.get_value() >> 21) & 0x1F;
 		m_ivs.special_defense	= (t_lcg.get_value() >> 26) & 0x1F;
-		m_ivs.speed				= (t_lcg.get_value() >> 16) & 0x1F;
+		m_ivs.speed		= (t_lcg.get_value() >> 16) & 0x1F;
 
 		//Calculate the HP type from the IVs
 		m_hidden_power.type_id = 
 		(
-			((m_ivs.hp				& 1) << 0) + 
-			((m_ivs.attack			& 1) << 1) +
-			((m_ivs.defense			& 1) << 2) +
-			((m_ivs.speed			& 1) << 3) +
+			((m_ivs.hp		& 1) << 0) + 
+			((m_ivs.attack		& 1) << 1) +
+			((m_ivs.defense		& 1) << 2) +
+			((m_ivs.speed		& 1) << 3) +
 			((m_ivs.special_attack	& 1) << 4) +
 			((m_ivs.special_defense	& 1) << 5)
 		) * 15 / 0x3F;
@@ -86,10 +86,10 @@ namespace gen4::pkm
 		//Calculate the HP power from the IVs
 		m_hidden_power.power =
 		(
-			(((m_ivs.hp					>> 1) & 1) << 0) +
-			(((m_ivs.attack				>> 1) & 1) << 1) +
-			(((m_ivs.defense			>> 1) & 1) << 2) +
-			(((m_ivs.speed				>> 1) & 1) << 3) +
+			(((m_ivs.hp			>> 1) & 1) << 0) +
+			(((m_ivs.attack			>> 1) & 1) << 1) +
+			(((m_ivs.defense		>> 1) & 1) << 2) +
+			(((m_ivs.speed			>> 1) & 1) << 3) +
 			(((m_ivs.special_attack		>> 1) & 1) << 4) +
 			(((m_ivs.special_defense	>> 1) & 1) << 5)
 		) * 40 / 0x3F + 30;
